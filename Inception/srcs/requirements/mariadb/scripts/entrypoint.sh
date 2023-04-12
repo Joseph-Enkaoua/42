06776311184edf980144a6e1cadd8f9f3dcf4475
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2023-04-12 09:33:47 +0200

    Inception

---
+++ Inception/srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -0,0 +1,11 @@
+cat .exist 2> /dev/null
+if [ $? -ne 0 ]; then
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
+    	sleep 1
+	done
+	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
+	touch .exist
+else
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql
+fi
